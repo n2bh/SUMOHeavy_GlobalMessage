a74a269deb7a781bf4b37fc8ab995746cf9e5a13
Sean Kennedy <sean@splitleaf.net>
2014-01-16 10:57:48 -0500

    Adding base styles and moving to community codepool

---
+++ app/code/community/SUMOHeavy/GlobalMessage/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * SUMOHeavy_GlobalMessage
+ *
+ * @category   SUMOHeavy
+ * @package    SUMOHeavy_GlobalMessage
+ * @copyright  Copyright 2013 SUMO Heavy Industries, LLC
+ * @author     Sean Kennedy <support@sumoheavy.com>
+ */
+class SUMOHeavy_GlobalMessage_Helper_Data
+extends Mage_Core_Helper_Abstract
+{
+}
